011a57fdd4278ae78ea87fb9ea1d9c7460f09c1f
HarryW217 <harrywalker217@gmail.com>
2023-10-05 10:46:37 +0100

    Changes made for hosting

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,21 @@
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
+const config = {}
+
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
+module.exports = new Pool(config);
 
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
+}
 
 if (!process.env.PGDATABASE) {
   throw new Error('PGDATABASE not set');
