30e78a4e732c395d9938606bfd759a7d7857f02e
HarryW217 <harrywalker217@gmail.com>
2023-10-05 10:53:28 +0100

    tweaked connection file

--- db/connection.js
+++ db/connection.js
@@ -1,24 +1,22 @@
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
-const config = {}
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
+const config = {};
+
+
+
+require("dotenv").config({
+  path: `${__dirname}/../.env.${ENV}`,
+});
 
 if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
 }
 
-module.exports = new Pool(config);
-
-require('dotenv').config({
-  path: `${__dirname}/../.env.${ENV}`,
-});
-
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
-}
 
-module.exports = new Pool();
+module.exports = new Pool(config);
+
