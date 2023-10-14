6535a5191ee3541321bcbf6016b2d2d85b0c616d
HarryW217 <harrywalker217@gmail.com>
2023-10-14 17:50:03 +0100

    Proof read repo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const { getTopics } = require("./controllers/topics-controllers");
 const { getEndPoints } = require("./controllers/api-controllers");
-const { getUsers }= require("./controllers/users-controllers")
+const { getUsers } = require("./controllers/users-controllers");
 const {
   getArticles,
   getArticleById,
@@ -10,8 +10,7 @@ const {
   patchArticlesById,
   postComment,
 } = require("./controllers/articles-controllers");
-
-const { deleteComment } = require("./controllers/comments-controllers")
+const { deleteComment } = require("./controllers/comments-controllers");
 
 app.use(express.json());
 
@@ -28,13 +27,13 @@ app.get("/api/articles/:article_id/comments", getArticleCommentsById);
 
 app.get("/api/users", getUsers);
 
-//POST requests
+//POST request
 app.post("/api/articles/:article_id/comments", postComment);
 
-// DELETE requests
+// DELETE request
 app.delete("/api/comments/:comment_id", deleteComment);
 
-//PATCH requests
+//PATCH request
 app.patch("/api/articles/:article_id", patchArticlesById);
 
 //Handle 404 errors
@@ -45,14 +44,11 @@ app.all("/*", (req, res) => {
 //Handle custom errors
 
 app.use((err, req, res, next) => {
-  if (err.code === "22P02") {
+  if (err.code === "22P02" || err.code === "23503") {
     res.status(400).send({ msg: "Invalid input" });
   }
   if (err.code === "23502") {
     res.status(400).send({ msg: "Missing required fields" });
-  }
-  if (err.code === "23503") {
-    res.status(400).send({ msg: "Invalid input" });
   }
   res.status(err.status).send({ msg: err.msg });
   next(err);
